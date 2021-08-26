48c059387137d37e791ac962f3d9267bf5546966
Christian Kirschberg <ckirschberg@gmail.com>
2021-08-26 11:21:40 +0200

    todo

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+
+  const handleAddTodo = () => {
+    setTodos(todos => [...todos, text]);
+    console.log(todos);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Our first Todo app</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+
+      <Button
+        title="Add Todo"
+        onPress={handleAddTodo}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +36,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
+
